915777ff728c66244f648ef836eb0abcdbbc7323
grsmith35 <gordonrileysmith@gmail.com>
2023-10-19 21:35:55 -0600

    adjusting tables

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/tattoo-app', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: 'budget-app'
 });
 
 module.exports = mongoose.connection;
